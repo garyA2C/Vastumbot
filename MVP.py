7eb2579b22ee9d19694629854359d94b8b6a298e
emmanuel <emmanuel.montblanc@insa-lyon.fr>
2021-11-24 16:21:45 +0100

    if it detects a person, blurs her

--- MVP.py
+++ MVP.py
@@ -1,16 +1,27 @@
 import cv2
 
-thres = 0.45  # Threshold to detect object
 
-# cap = cv2.VideoCapture(1)
-# cap.set(3,1280)
-# cap.set(4,720)
-# cap.set(10,70)
-video_path = r"C:\Users\Emmanuel\Videos\tuto lotus p2\YouCut_20200521_181116375.mp4"
+def blur_person(image, factor=3.0):
+    (h, w) = image.shape[:2]
+    kW = int(w / factor)
+    kH = int(h / factor)
+    # ensure the width of the kernel is odd
+    if kW % 2 == 0:
+        kW -= 1
+    # ensure the height of the kernel is odd
+    if kH % 2 == 0:
+        kH -= 1
+    # apply a Gaussian blur to the input image using our computed
+    # kernel size
+    return cv2.GaussianBlur(image, (kW, kH), 0)
+
+
+thres = 0.60  # Threshold to detect object
+
+# video_path = r"C:\Users\Emmanuel\Videos\tuto lotus p2\YouCut_20200521_181116375.mp4"
 # cap = cv2.VideoCapture(video_path)
 cap = cv2.VideoCapture(0)
 
-classNames = []
 classFile = 'coco.names'
 with open(classFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
@@ -32,17 +43,21 @@ while True:
     if len(classIds) != 0:
         for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
             try:
+                x, y, w, h = box
                 cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
                 cv2.putText(img, classNames[classId - 1].upper(), (box[0], box[1]),
                             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
                 cv2.putText(img, str(round(confidence * 100, 2)), (box[0], box[1]),
                             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+                # If a person is detected, blur her
+                if classId == 1:
+                    person_img = img[y:y+h, x:x+w]
+                    blured_person = blur_person(person_img)
+                    img[y:y+h, x:x+w] = blured_person
             except:
                 print("error")
-                # print(classNames[classId - 1].upper())
-                # print(box)
 
-    cv2.imshow('Output', img)
     cv2.imshow('Output', img)
     cv2.waitKey(1)
