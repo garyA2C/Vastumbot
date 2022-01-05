7015f6d025f2c9d375c5e02e443f0908eb004efa
Andrei Vadineanu <andrei.vadineanu@yahoo.com>
2022-01-05 15:34:41 +0100

    Test connection with gopro

--- MVP.py
+++ MVP.py
@@ -1,5 +1,11 @@
 import cv2
+import time
+from collections import Counter
 
+from goprocam import GoProCamera
+from goprocam import constants
+# gopro = GoProCamera.GoPro()
+# gopro.stream("udp://127.0.0.1:10000")
 
 def blur_person(image, factor=3.0):
     (h, w) = image.shape[:2]
@@ -20,7 +26,9 @@ thres = 0.60  # Threshold to detect object
 
 # video_path = r"C:\Users\Emmanuel\Videos\tuto lotus p2\YouCut_20200521_181116375.mp4"
 # cap = cv2.VideoCapture(video_path)
-cap = cv2.VideoCapture(0)
+gpCam = GoProCamera.GoPro()
+cap = cv2.VideoCapture("udp://127.0.0.1:10000")
+# cap = cv2.VideoCapture(0)
 
 classFile = 'coco.names'
 with open(classFile, 'rt') as f:
@@ -35,6 +43,7 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
+det_info = []
 while True:
     success, img = cap.read()
     classIds, confs, bbox = net.detect(img, confThreshold=thres)
@@ -42,22 +51,34 @@ while True:
 
     if len(classIds) != 0:
         for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            try:
+            # try:
+                if len(det_info) < 10:
+                    det_info.append((classNames[classId-1].upper(), round(confidence*100,2)))
+                
+                    # for idx, val in enumerate(det_info):
+                    #     print(idx, val)
+
                 x, y, w, h = box
                 cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                cv2.putText(img, classNames[classId - 1].upper(), (box[0], box[1]),
+                # cv2.putText(img, classNames[classId - 1].upper(), (box[0], box[1]),
+                #             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+                # cv2.putText(img, str(round(confidence * 100, 2)), (box[0], box[1]),
+                #             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(img, det_info[0][0], (box[0], box[1]),
                             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-                cv2.putText(img, str(round(confidence * 100, 2)), (box[0], box[1]),
+                cv2.putText(img, str(det_info[0][1]), (box[0], box[1]),
                             cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-
+                if len(det_info) >= 10:
+                    det_info = []
                 # If a person is detected, blur her
                 if classId == 1:
                     person_img = img[y:y+h, x:x+w]
                     blured_person = blur_person(person_img)
                     img[y:y+h, x:x+w] = blured_person
-            except:
-                print("error")
+            # except:
+            #     print("error")
 
     cv2.imshow('Output', img)
     cv2.waitKey(1)
