866c755089a05379184301fd0ca3f919b0c125fa
Andrei Vadineanu <andrei.vadineanu@yahoo.com>
2022-01-13 04:43:50 +0100

    Ties detection to backend

--- Backend/postprocessing.py
+++ Backend/postprocessing.py
@@ -1,27 +1,24 @@
 import os
+import pandas as pd
+import re
+from datetime import datetime
+from dateutil import parser
+import calendar
+import json
+import shutil
 
-test = [(1, 404, 3, 69, 50), (2, 394, 4, 143, 221), (3, 173, 1, 42, 28), (4, 164, 18, 37, 28), (5, 163, 21, 29, 24), (6, 79, 146, 53, 43), (7, 45, 192, 68, 46), (8, 11, 299, 63, 52), (9, 535, 0, 27, 29), (10, 536, 2, 23, 36), (11, 538, 4, 22, 40), (12, 541, 6, 24, 43), (13, 544, 11, 24, 41), (14, 547, 20, 22, 41), (15, 553, 24, 18, 41), (16, 553, 32, 23, 36), (17, 554, 29, 23, 40), (18, 555, 34, 22, 51), (19, 557, 42, 22, 46), (20, 566, 55, 26, 48), (21, 561, 57, 21, 46), (22, 570, 69, 24, 48), (23, 576, 70, 25, 50), (24, 585, 74, 21, 53), (25, 591, 85, 22, 53), (26, 594, 90, 30, 50), (27, 606, 106, 29, 53), (28, 72, 0, 44, 41), (29, 47, 47, 50, 40), (30, 30, 63, 54, 48), (31, 30, 69, 49, 48), (32, 16, 87, 54, 54), (33, 9, 93, 61, 57), (34, 7, 101, 59, 57), (35, 385, 3, 99, 63), (36, 547, 211, 68, 73), (37, 555, 231, 77, 80)]
 
+detection = pd.read_csv('output.csv')
 idx = []
-idx2 = []
-
 
 # Create function to filter out the pictures which didn't have significant changes compared to the previous
-for i in range(len(test)):
+for i in range(len(detection)):
     try:
-        before = test[i-1][1] + test[i-1][2] + test[i-1][3] + test[i-1][4]
-        now = test[i][1] + test[i][2] + test[i][3] + test[i][4]
-        after = test[i+1][1] + test[i+1][2] + test[i+1][3] + test[i+1][4]
-
-        coord = (test[i][1] - test[i+1][2])**2 + (test[i][2] - test[i+1][2])**2
-        area = (test[i][3] - test[i+1][3])**2 + (test[i][4] - test[i+1][4])**2
+        now = detection['x'][i] + detection['y'][i] + detection['h'][i] + detection['w'][i]
+        after = detection['x'][i+1] + detection['y'][i+1] + detection['h'][i+1] + detection['w'][i+1]
         if abs(after-now) > 100:
-            # print(test[i][0], abs(before-now), abs(after-now))
-            idx.append(test[i][0])
-        if coord > 10000 and area > 1000:
-            # print(test[i][0], coord, area)
-            pass
-    except(IndexError):
+            idx.append(detection['object'][i])
+    except:
         pass
 
 # Split array into arrays of consecutive indexes
@@ -43,19 +40,89 @@ def split_non_consequtive(data):
 # Select image in th middle of each array containing consecutive indexes
 final_idx = []
 for i in split_non_consequtive(idx):
-    middleIndex = round(sum(i)/len(i))
+    middleIndex = round(sum(i)/len(i)-1)
     final_idx.append(middleIndex)
 
-print(final_idx)
 # Delete contents of filtered images
 arr = os.listdir('output')
+arr.sort(key=lambda f: int(re.sub('\D', '', f)))
+path = '../Server/data/'
+if not os.path.exists(path):
+    os.makedirs(path)
+new_number = 0
 for i in arr:
     if i.endswith(".jpg"):
         delete = True
         for j in final_idx:
             if int(i[:-4]) == j:
                 delete = False
+                new_path = path + str(new_number)
+                if not os.path.exists(new_path):
+                    os.makedirs(new_path)
+                    shutil.copy2('output/'+str(j)+'.jpg', new_path+'/image.jpg')
+                    shutil.copy2('output/'+str(j)+'.txt', new_path+'/info.txt')
+                new_number += 1
+        if delete == True:
+            detection['object'][int(i[:-4])] = -1
     if delete == True:
-        os.remove("output/" + i)
-        # print(i)
-        # pass
+        # os.remove("output/" + i)
+        pass
+
+gps = pd.read_csv('test_velo_full.csv')
+gps = gps.rename(columns=({'date': 'timestamp', 'GPS (Lat.) [deg]':'lat', 'GPS (Long.) [deg]': 'lon'}))
+del gps['cts'], gps['precision'], gps['fix'], gps['altitude system'], gps['GPS (Alt.) [m]'], gps['GPS (2D speed) [m/s]'], gps['GPS (3D speed) [m/s]'] 
+del detection['x'], detection['y'], detection['w'], detection['h'], detection['frame']
+
+len_det = len(detection)
+len_gps = len(gps)
+
+detection['lat'] = float(0)
+detection['lon'] = float(0)
+detection['timestamp'] = int(0)
+detection['path'] = 'data/'
+detection['status'] = 'active'
+detection['id_user'] = 1
+detection.replace('umbrella', 'nonrecyclable', inplace=True)
+detection.replace('bottle', 'glass', inplace=True)
+
+for idx_det in final_idx:
+    idx_gps = round(idx_det*len_gps/len_det)
+    temp = parser.parse(gps['timestamp'][idx_gps])
+    timestamp = calendar.timegm(temp.timetuple())
+    detection['timestamp'][idx_det] = int(timestamp)
+    detection['lon'][idx_det] = gps['lon'][idx_gps] 
+    detection['lat'][idx_det] = gps['lat'][idx_gps]
+
+detection = detection[detection.object != -1]
+detection = detection.reset_index(drop=True)
+
+detection = detection[['path', 'lat', 'lon', 'timestamp', 'type', 'status', 'id_user']]
+for i in range(len(detection)):
+    detection['path'][i] = str(detection['path'][i]) + str(i) + '/image.png'
+
+for i in range(len(detection)):
+    data = {}
+    data['path'] = str(detection['path'][i])
+    data['lat'] = float(detection['lat'][i])
+    data['lon'] = float(detection['lon'][i])
+    data['timestamp'] = int(detection['timestamp'][i])
+    data['type'] = str(detection['type'][i])
+    data['status'] = str(detection['status'][i])
+    data['id_user'] = int(detection['id_user'][i])
+    new_path = path + str(i)
+    with open(new_path+'/info.txt', 'w') as file:
+        json.dump(data, file)
+
+big_data = {}
+for i in range(len(detection)):
+    big_data[i] = {}
+    big_data[i]['path'] = str(detection['path'][i])
+    big_data[i]['lon'] = float(detection['lat'][i])
+    big_data[i]['lat'] = float(detection['lon'][i])
+    big_data[i]['timestamp'] = int(detection['timestamp'][i])
+    big_data[i]['type'] = str(detection['type'][i])
+    big_data[i]['status'] = str(detection['status'][i])
+    big_data[i]['id_user'] = int(detection['id_user'][i])
+new_path = '../Server/'
+with open(new_path+'data.json', 'w') as file:
+    json.dump(big_data, file)
